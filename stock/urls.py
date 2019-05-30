8b44f277a94f0beea0bcdfeabe10448adb814c8d
qurg <qurg@outlook.com>
2019-05-30 15:10:57 +0800

    Revert "增加定时任务功能"
    This reverts commit 24b592fc

--- stock/urls.py
+++ stock/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
     # path('', admin.site.urls),
-    path('update/', views.index)
 ]
--- stock/views.py
+++ stock/views.py
@@ -1,11 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
-from stock.tasks import update_data
-
-
-def index(request):
-    update_data.delay(1, '000002')
-    return HttpResponse('Hello Celery')
